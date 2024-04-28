0faecf734af47804f3c6405be7f9a9888e9adc4d
Shadman <shadman.alizada@gmail.com>
2024-04-28 23:58:56 +0400

    refactor: some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './container/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        container: {
-            center: true,
-            padding: '2rem',
-            screens: {
-                '2xl': '1334px',
-            },
-        },
-        extend: {
-            fontFamily: {
-                berlinSans: ['var(--font-berlin-sans)'],
-                dmSans: ['var(--font-dm-sans)'],
-            },
-            colors: {
-                primary: '#45f882',
-                secondary: '#ffbe18',
-                'text-gray-clr': '#9b9b9b',
-
-                'primary-hover': '#7444ff',
-                'h-clr': ' #131740',
-                't-clr': ' #5A5D79',
-                'dark-clr': '#0D102D',
-
-            },
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./container/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1334px",
+      },
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        berlinSans: ["var(--font-berlin-sans)"],
+        dmSans: ["var(--font-dm-sans)"],
+      },
+      colors: {
+        primary: "#45f882",
+        secondary: "#ffbe18",
+        tertiary: "#1c242f",
+        "t-gray-clr": "#ADB0BC",
+        "dark-clr": "#0B0E13",
+
+        "primary-hover": "#7444ff",
+        "h-clr": " #131740",
+        "t-clr": " #5A5D79",
+      },
+      animation: {
+        "move-y": "move-y 5s ease-in-out infinite",
+      },
+      keyframes: {
+        "move-y": {
+          "0%, 100%": { transform: "translateY(-10px)" },
+          "50%": { transform: "translateY(10px)" },
+        },
+      },
+    },
+  },
+  plugins: [],
 };
