d0067be20ae5d5bc6eb421d012f865f2e74a3f67
Shadman <shadman.alizada@gmail.com>
2024-04-28 23:58:11 +0400

    chore: fonts and svgs added

---
+++ container/Home/data/icons.js
@@ -0,0 +1,94 @@
+export const ButtonPrimary = ({ className }) => {
+  return (
+    <svg
+      preserveAspectRatio="none"
+      viewBox="0 0 197 60"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentcolor"
+      stroke="#45f882"
+      className={className}
+    >
+      <path
+        class="cls-1"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M30.976 0.755987L0.75499 30.977L29.717 58.677H165.717L195.938 30.977L165.717 0.755987H30.976Z"
+        stroke="#45f882"
+        stroke-width="1.5"
+      ></path>
+      <path
+        class="cls-2"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M166.712 2.01899L196.933 30.98L166.712 58.68L188.118 29.719L166.712 2.01899Z"
+        fill="#45f882"
+      ></path>
+      <path
+        class="cls-2"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        stroke="#45f882"
+        d="M30.235 2.01899L0.0139923 30.977L30.235 58.677L8.82899 29.719L30.235 2.01899Z"
+        fill="#45f882"
+      ></path>
+    </svg>
+  );
+};
+export const ButtonBg = ({ className }) => {
+  return (
+    <svg
+      preserveAspectRatio="none"
+      viewBox="0 0 197 60"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentcolor"
+      stroke="currentcolor"
+      className={className}
+    >
+      <path
+        class="cls-1"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M30.976 0.755987L0.75499 30.977L29.717 58.677H165.717L195.938 30.977L165.717 0.755987H30.976Z"
+        stroke="white"
+        stroke-width="1.5"
+      ></path>
+      <path
+        class="cls-2"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M166.712 2.01899L196.933 30.98L166.712 58.68L188.118 29.719L166.712 2.01899Z"
+        fill="white"
+      ></path>
+      <path
+        class="cls-2"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M30.235 2.01899L0.0139923 30.977L30.235 58.677L8.82899 29.719L30.235 2.01899Z"
+        fill="white"
+      ></path>
+    </svg>
+  );
+};
+
+export const ButtonBg2 = ({ className }) => {
+  return (
+    <svg
+      preserveAspectRatio="none"
+      viewBox="0 0 167 53"
+      fill="none"
+      stroke="currentcolor"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path d="M166 1H1V52H166V1Z" stroke-width="2"></path>
+      <path d="M166 22L161 27L166 33" stroke-width="2"></path>
+      <path d="M166 14L154 28L166 40" stroke-width="2"></path>
+      <path d="M8 1V21" stroke-width="2"></path>
+      <path d="M159 1V22" stroke-width="2"></path>
+      <path d="M159 33V52" stroke-width="2"></path>
+      <path d="M8 34V52" stroke-width="2"></path>
+      <path d="M1 22L8 27L1 33" stroke-width="2"></path>
+      <path d="M1 14L15 28L1 40" stroke-width="2"></path>
+    </svg>
+  );
+};
